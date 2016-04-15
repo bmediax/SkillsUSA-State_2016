d4236e5bf823a89a0b0cc3c1a35b5cb65234ff31
iSchools Design <ischools@ischoolss-mbp.benson.ppsnet>
2016-04-15 08:18:08 -0700

    Intro about

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ $(document).ready(function() {
         $("nav ul").slideToggle();
     });
 });
+
+// Smooth Scrolling for #
+$(function() {
+
+    // Selecting only a href to a #
+  $('a[href*="#"]:not([href="#"])').click(function() {
+
+      // Looking for the path
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+      // Will animte the distance
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
