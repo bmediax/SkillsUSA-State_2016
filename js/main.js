005811d9fa967acc9c1b73b925cef8d6120a91a3
iSchools Design <ischools@ischoolss-mbp.benson.ppsnet>
2016-04-15 07:48:06 -0700

    Intro > Responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+    // If Mobile Nav menu clicked then Navigation Nav UL Slide Down
     $("#mobile-nav").click(function() {
+        // Slide Toggle Nav ul
         $("nav ul").slideToggle();
     });
 });
